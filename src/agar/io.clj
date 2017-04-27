5033781efc8502defb5ce8edbb4bc5b5145aea71
pedro <pedro.m4rtins@gmail.com>
2017-04-27 01:01:39 -0300

    add spectation

--- src/agar/io.clj
+++ src/agar/io.clj
@@ -7,10 +7,12 @@
   (:use [clojure.core.matrix.operators :only [+ - * /]]))
 
 (derive ::start ::frozen)
+(derive ::start ::spectation)
 (derive ::pause ::frozen)
-(derive ::game-over ::frozen)
+(derive ::game-over ::active)
+(derive ::game-over ::spectation)
 (s/def ::state #{::start ::active ::pause ::game-over})
-(s/def ::transition #{[::start ::active]
+(s/def ::transition #{[::start ::active] ;http://www.61131.com/sfchowto.htm
                       [::active ::pause]
                       [::pause ::active]
                       [::active ::game-over]
@@ -32,6 +34,7 @@
     {:state ::start
      :count (+ 1 ai-count)
      :zoom 2
+     :viewpoint (cycle (range (+ ai-count 1)))
      :position (vec (cons (/ size 2) ai-position))
      :diameter (vec (cons 20 ai-diameter))
      :color (vec (cons [255 255 255] ai-color))
@@ -58,8 +61,8 @@
     #(let [distance-vector (- (position %2) (position %1))
            distance² (m/magnitude-squared distance-vector)]
       (* (diameter %2)
-         (/ distance-vector (+ distance² 1))
-         (- (diameter %1) (diameter %2))))))
+         (- (diameter %1) (diameter %2))
+         (/ distance-vector (+ distance² 1))))))
 
 (defn check [{:keys [diameter] :as new} {:keys [versor] :as old}]
   (-> new
@@ -89,30 +92,33 @@
   (apply q/fill c)
   (q/ellipse x y d d))
 
-(defn draw! [{:keys [state zoom] [player-position _] :position :as game}]
+(defn draw! [{:keys [state zoom viewpoint position] :as game}]
   ;zoom!
   (q/push-matrix)
   (q/translate (screen-center 0) (screen-center 1))
   (q/scale zoom)
   (q/translate (- (screen-center 0))
                (- (screen-center 1)))
-  ;in-game!
-  (q/with-translation (- screen-center player-position)
-    ;grid!
-    (q/background 30)
-    (q/stroke 255)
-    (doseq [bar (range 0 (+ game-size 0.1) (* screen-width 0.05))]
-      (q/line bar 0 bar game-size)
-      (q/line 0 bar game-size bar))
-    ;cells!
-    (doall (map cell! (sort-by :diameter (soa->aos game)))))
-  ;screen!
+  ;viewpoint!
+  (let [view (if (isa? state ::spectation) (first viewpoint) 0)]
+    ;in-game!
+    (q/with-translation (- screen-center (position view))
+      ;grid!
+      (q/background 30)
+      (q/stroke 255)
+      (doseq [bar (range 0 (+ game-size 0.1) (* screen-width 0.05))]
+        (q/line bar 0 bar game-size)
+        (q/line 0 bar game-size bar))
+      ;cells!
+      (doall (map cell! (sort-by :diameter (soa->aos game))))))
   (q/pop-matrix)
+  ;status!
   (when-not (= state ::active)
     (q/fill 0 255 0)
     (q/text-size 20)
-    (q/text (str state) 5 20)
-    ;helpers!
+    (q/text (str state) 5 20))
+  ;helpers!
+  (when (isa? state ::spectation)
     (let [mouse [(q/mouse-x) (q/mouse-y)]]
       ;distance!
       (q/stroke 255 155 0)
@@ -123,7 +129,7 @@
               5 (- screen-height 10)))))
 
 (defn player-rotates [game {:keys [x y]}]
-  (if (isa? (:state game) ::active)
+  (if (= (:state game) ::active)
     (assoc-in game [:versor 0] (m/normalise (- [x y] screen-center)))
     game))
 
@@ -137,19 +143,22 @@
     (assoc game :state ::active)
     game))
 
-(defn io-handle [game {key :key-code}] ;todo: start-menu, spacebar leap
+(defn key-handle [game {key :key-code}] ;todo: start-menu, spacebar leap
   (case (:state game)
         ::active (case key 10 (assoc game :state ::pause) game)
         ::pause (case key 10 (assoc game :state ::active) game)
         ::start (case key 10 (assoc game :state ::active) game)
         ::game-over (case key 10 (new-game)) game))
 
-(defn cycle-state [game _] ;todo*: bound transition to certain action
-  (let [transit-map (group-by first (s/form ::transition))
-        transit-set (-> game :state transit-map set)]
-    (if (= (count transit-set) 1)
-      (assoc game :state (-> transit-set first second))
-      game)))
+(defn click-handle [{:keys [state viewpoint] :as game} _]
+  (cond-> game
+    true (update :viewpoint next)
+    (isa? state ::active) (assoc :state ::game-over)))
+
+(defn zoom-wheel [game spin]
+  (if (isa? (:state game) ::spectation)
+    (update game :zoom + (* spin 0.1))
+    game))
 
 (q/defsketch agar.io
   :size [screen-width screen-height]
@@ -159,6 +168,7 @@
   :mouse-moved player-rotates
   :mouse-exited pause
   :mouse-entered unpause
-  :key-pressed io-handle
-  :mouse-pressed cycle-state
+  :key-pressed key-handle
+  :mouse-pressed click-handle
+  :mouse-wheel zoom-wheel
   :middleware [qm/fun-mode])
